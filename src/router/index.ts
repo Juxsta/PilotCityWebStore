354410f8d322725e9fb953debb468c204a4fd29a
jinocenc <jerold.inocencio@gmail.com>
2020-07-01 05:57:56 -0700

    :zap: created functional routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,12 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    redirect: { name: 'Shop' },
   },
   {
     path: '/about',
@@ -22,6 +20,11 @@ const routes: Array<RouteConfig> = [
     path: '/shop',
     name: 'Shop',
     component: () => import('@/views/Shop.vue'),
+  },
+  {
+    path: '/product/:product_id',
+    name: 'Product',
+    component: () => import('../views/Product.vue'),
   },
 ];
 
